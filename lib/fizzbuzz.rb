5ce933a34777ce8039471982308f5f925af5cfe9
Maria Vanaale <mvanaale@gmail.com>
2019-01-30 12:59:18 +0000

    fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,21 +1,33 @@
-describe 'fizzbuzz' do
-  it 'return "fizz" when passes 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  else number % 5 == 0
+    'buzz'
   end
-  it 'returns "fizz" when passed 6' do
-    expect(fizbuzz(6)).to eq "fizz"
-  end
-  it 'returns "buzz" when passed 5' do
-    expect(fizbuzz(5)).to eq "buzz"
-  end
-  it 'returns 7 when passed 7' do
-    expect(fizbuzz(7)).to eq 7
-  end
-  it 'returns fizzbuzz when passed 15' do
-    expect(fizbuzz(15)).to eq "fizzbuzz"
-  end
-  it 'returns buzz when passed 10' do
-    expect(fizbuzz(10)).to eq "buzz"
-  end
-
 end
+
+
+
+# describe 'fizzbuzz' do
+#   it 'return "fizz" when passes 3' do
+#     expect(fizzbuzz(3)).to eq 'fizz'
+#   end
+#   it 'returns "fizz" when passed 6' do
+#     expect(fizbuzz(6)).to eq "fizz"
+#   end
+#   it 'returns "buzz" when passed 5' do
+#     expect(fizbuzz(5)).to eq "buzz"
+#   end
+#   it 'returns 7 when passed 7' do
+#     expect(fizbuzz(7)).to eq 7
+#   end
+#   it 'returns fizzbuzz when passed 15' do
+#     expect(fizbuzz(15)).to eq "fizzbuzz"
+#   end
+#   it 'returns buzz when passed 10' do
+#     expect(fizbuzz(10)).to eq "buzz"
+#   end
+#
+# end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,22 +1,18 @@
 require 'fizzbuzz'
-describe 'fizzbuzz' do
-  it 'return "fizz" when passes 3' do
+describe  "fizzbuzz" do
+  it 'returns "fizz" when passes 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  # it 'returns "fizz" when passed 6' do
-  #   expect(fizbuzz(6)).to eq "fizz"
-  # end
-  # it 'returns "buzz" when passed 5' do
-  #   expect(fizbuzz(5)).to eq "buzz"
-  # end
-  # it 'returns 7 when passed 7' do
-  #   expect(fizbuzz(7)).to eq 7
-  # end
-  # it 'returns fizzbuzz when passed 15' do
-  #   expect(fizbuzz(15)).to eq "fizzbuzz"
-  # end
-  # it 'returns buzz when passed 10' do
-  #   expect(fizbuzz(10)).to eq "buzz"
-  # end
-
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
